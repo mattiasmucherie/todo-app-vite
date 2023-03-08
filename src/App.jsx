b9300858d653c6ceaa32e8904c2f4718d32f4163
Mattias Mucherie <mattias.mattias.m@gmail.com>
2023-03-08 18:25:57 +0100

    Solution up to task 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
 import "./App.css";
 
-function ListItem({ todo, onRemove }) {
+function ListItem(props) {
+  const { todo, onRemove } = props;
   // Implement the ListItem component
+  return (
+    <li className={todo.done ? "Done" : "NotDone"}>
+      {todo.task} <button onClick={onRemove}>Done</button>
+    </li>
+  );
 }
 
 function App() {
-  const [todos, setTodos] = useState(["Buy milk", "Go to gym"]);
+  const [todos, setTodos] = useState([
+    { task: "Buy milk", done: false },
+    { task: "Go to gym", done: true },
+  ]);
+
   const [textField, setTextfield] = useState("");
 
   // Implement the addTodo function
-
+  const addTodo = () => {
+    if (textField) {
+      setTodos([...todos, { task: textField, done: false }]);
+      setTextfield("");
+    }
+  };
   // Implement the handleRemove function
+  const handleRemove = (oneTodo) => {
+    const filteredTodos = todos.filter((todo) => todo.task !== oneTodo.task);
+    oneTodo.done = true;
+
+    setTodos([...filteredTodos, oneTodo]);
+  };
 
   const myTodos = todos.map((todo, key) => (
-    <ListItem key={key} todo={todo} onRemove={handleRemove} />
+    <ListItem key={key} todo={todo} onRemove={() => handleRemove(todo)} />
   ));
 
   return (
@@ -25,6 +46,7 @@ function App() {
           value={textField}
           onChange={(e) => {
             // Implement the onChange handler for the input field
+            setTextfield(e.target.value);
           }}
         />
         <button onClick={addTodo}>Add</button>
