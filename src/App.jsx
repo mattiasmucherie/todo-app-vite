60a9eb6a6cc1e91be9e51f64334dc8345bfd3728
Mattias Mucherie <mattias.mattias.m@gmail.com>
2023-03-09 10:54:41 +0100

    Task 4 and 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,75 @@
 import { useState } from "react";
 import "./App.css";
 
+const categories = ["Personal", "Home", "Work"];
+
 function ListItem(props) {
   const { todo, onRemove } = props;
   // Implement the ListItem component
   return (
     <li className={todo.done ? "Done" : "NotDone"}>
-      {todo.task} <button onClick={onRemove}>Done</button>
+      {todo.task} | {todo.category} <button onClick={onRemove}>Done</button>
     </li>
   );
 }
 
 function App() {
   const [todos, setTodos] = useState([
-    { task: "Buy milk", done: false },
-    { task: "Go to gym", done: true },
+    { task: "Buy milk", done: false, category: "home" },
+    { task: "Go to gym", done: true, category: "personal" },
   ]);
 
-  const [textField, setTextfield] = useState("");
+  const [formData, setFormData] = useState({ todo: "", category: "home" });
 
   // Implement the addTodo function
   const addTodo = () => {
-    if (textField) {
-      setTodos([...todos, { task: textField, done: false }]);
-      setTextfield("");
+    if (formData.todo && formData.category) {
+      setTodos([
+        ...todos,
+        { task: formData.todo, done: false, category: formData.category },
+      ]);
+
+      setFormData({ todo: "", category: "" });
     }
   };
   // Implement the handleRemove function
   const handleRemove = (oneTodo) => {
-    const filteredTodos = todos.filter((todo) => todo.task !== oneTodo.task);
-    oneTodo.done = true;
+    const updatedTodos = [...todos];
+    const index = updatedTodos.findIndex((t) => t.task === oneTodo.task);
+    updatedTodos[index].done = !updatedTodos[index].done;
+    setTodos(updatedTodos);
+  };
 
-    setTodos([...filteredTodos, oneTodo]);
+  const handleCategoryChange = (event) => {
+    setFormData({ ...formData, category: event.target.value });
   };
 
   const myTodos = todos.map((todo, key) => (
     <ListItem key={key} todo={todo} onRemove={() => handleRemove(todo)} />
   ));
 
+  const myOptions = categories.map((category) => {
+    return (
+      <option key={category} value={category.toLowerCase()}>
+        {category}
+      </option>
+    );
+  });
   return (
     <div className="App">
       <div>
         <input
           type="text"
-          value={textField}
+          value={formData.todo}
           onChange={(e) => {
             // Implement the onChange handler for the input field
-            setTextfield(e.target.value);
+            setFormData({ ...formData, todo: e.target.value });
           }}
         />
+        <select onChange={handleCategoryChange} value={formData.category}>
+          <option disabled>Choose a category</option>
+          {myOptions}
+        </select>
         <button onClick={addTodo}>Add</button>
       </div>
       <ul>{myTodos}</ul>
